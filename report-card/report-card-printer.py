6767e21ec6d0f3bb8a8aa9ce027058eaf21d9ba6
Otavio Braz <otaviobrazs01@gmail.com>
2026-04-23 21:18:00 -0300

    Update report-card-printer.py

--- report-card/report-card-printer.py
+++ report-card/report-card-printer.py
@@ -1,16 +1,12 @@
-#Verifica o nome do estudante pela variavel name e retorna com um print
 name = 'Alice'
 print(name, type(name))
 
-#Verifica se é um estudante pela variavel is_student e retorna com um print
 is_student = True
 print(is_student, type(is_student))
-
-#Verifica a idade pela variável age e retorna com um print 
+ 
 age = 20
 print(age, type(age))
 
-#Verifica o score do estudante e retorna com um print para o boletim
 score = 80.5
 print(isinstance(score, float))
 print(score, type(score))
